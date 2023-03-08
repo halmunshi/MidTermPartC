cd2ba80691dc6a08f8718efb4ac1495680287807
halmunshi <122491031+halmunshi@users.noreply.github.com>
2023-03-08 16:22:29 -0500

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -15,6 +15,7 @@ package midtermexam_versiona_extensioncode;
  */
 public class UserProfile 
 {
+    //Midterm Part B, Question 5 comment
     
     private String userID;//the userID
     private String genre;// the user's preferred genre of movie
